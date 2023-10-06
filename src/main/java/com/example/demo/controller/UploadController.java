edb9dc11ba361ba04cd886f5bb0c545bdd43b3d2
zhangyuhan <605286079@qq.com>
2023-10-06 17:11:00 -0400

    add interceptor for user login

---
+++ src/main/java/com/example/demo/controller/UploadController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@RestController
+public class UploadController {
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public String upload(MultipartFile file, HttpServletRequest http) throws IOException {
+        System.out.println(file.getOriginalFilename());
+        System.out.println(file.getContentType());
+        String path = http.getServletContext().getRealPath("/upload");
+
+        System.out.println(path);
+        saveFile(file, path);
+
+        return "upload success";
+    }
+
+    private void saveFile(MultipartFile file, String path) throws IOException{
+        File dir = new File(path);
+        if (!dir.exists()){
+            dir.mkdir();
+        }
+        File file1 = new File(path+"\\a.jpg");
+        System.out.println(file1.getName());
+        file.transferTo(file1);
+    }
+}
