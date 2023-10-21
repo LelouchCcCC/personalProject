6337fab8af3c7cef90617a5d12a45efd6723f427
zhangyuhan <605286079@qq.com>
2023-10-21 17:41:14 -0400

    return the basic message back

--- src/main/java/com/example/demo/controller/BasicController.java
+++ src/main/java/com/example/demo/controller/BasicController.java
@@ -1,8 +1,16 @@
 package com.example.demo.controller;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.demo.mapper.BasicMapper;
 import com.example.demo.urlRoute.Basic;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,12 +18,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
+@Tag(name = "BasicController", description = "relate to basic message of the user")
 public class BasicController {
     @Autowired
     private BasicMapper basicMapper;
+    @Operation(summary = "getBasicInformationByName", description = "fetch information depend on name")
+    @Parameter(name = "name",description = "who want to get information")
     @PostMapping("/basic-message")
+    @ApiResponse(description = "the response type",content = {
+            @Content(
+                    schema = @Schema(implementation = Basic.class)
+            )
+    })
     public Basic getBasicInformationByName(String name){
         Basic b = basicMapper.queryByName(name);
+        System.out.println(b);
         return b;
     }
 }
--- src/main/java/com/example/demo/urlRoute/Basic.java
+++ src/main/java/com/example/demo/urlRoute/Basic.java
@@ -1,16 +1,108 @@
 package com.example.demo.urlRoute;
 
-import java.util.Date;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.util.Date;
+@Schema(description = "Basic information for the user")
 public class Basic {
+    @Schema(description = "user name")
     private String name;
+    @Schema(description = "date of birth")
     private Date birth;
+    @Schema(description = "studying school")
     private String School;
-    private int phone;
-    private int subphone;
+    @Schema(description = "main phone number")
+    private Double phone;
+    @Schema(description = "sub phone number")
+    private Double subphone;
+    @Schema(description = "main mail")
     private String mailbox;
+    @Schema(description = "sub mail")
     private String submailbox;
+    @Schema(description = "personal photo")
     private String photo;
+    @Schema(description = "github link")
     private String githubLink;
+    @Schema(description = "linkedIn link")
     private String linkedInLink;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public String getSchool() {
+        return School;
+    }
+
+    public void setSchool(String school) {
+        School = school;
+    }
+
+    public Double getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Double phone) {
+        this.phone = phone;
+    }
+
+    public Double getSubphone() {
+        return subphone;
+    }
+
+    public void setSubphone(Double subphone) {
+        this.subphone = subphone;
+    }
+
+    public String getMailbox() {
+        return mailbox;
+    }
+
+    public void setMailbox(String mailbox) {
+        this.mailbox = mailbox;
+    }
+
+    public String getSubmailbox() {
+        return submailbox;
+    }
+
+    public void setSubmailbox(String submailbox) {
+        this.submailbox = submailbox;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getGithubLink() {
+        return githubLink;
+    }
+
+    public void setGithubLink(String githubLink) {
+        this.githubLink = githubLink;
+    }
+
+    public String getLinkedInLink() {
+        return linkedInLink;
+    }
+
+    public void setLinkedInLink(String linkedInLink) {
+        this.linkedInLink = linkedInLink;
+    }
 }
